b66cf49d21128c59ef8a8997d7133a8919d679eb
techbydeepak <deepakrathour05tech@gmail.com>
2025-08-11 12:36:56 +0530

    Add Vite base and prepare for GH Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+  base: '/namandigital_task3_e_com/' // ← add this line
 })
